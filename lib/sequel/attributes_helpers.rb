ce60174af7f8bfd75995613208b5fa60cb3460a4
Danny Whalen <daniel.r.whalen@gmail.com>
2014-05-19 11:24:41 -0700

    Refactor attributes helpers to reduce duplication

--- lib/sequel/attributes_helpers.rb
+++ lib/sequel/attributes_helpers.rb
@@ -3,18 +3,15 @@ module Sequel
   module Plugins
     module AttributesHelpers
       module InstanceMethods
-        def allowed_attributes
-          self.class.allowed_columns.reduce({}) do |res, attribute|
-            res[attribute] = self.public_send(attribute)
-            res
-          end
-        end
-
         def attributes
           self.class.columns.reduce({}) do |res, attribute|
             res[attribute] = self.public_send(attribute)
             res
           end
+        end
+
+        def allowed_attributes
+          attributes.select { |k,_| self.class.allowed_columns.include?(k) }
         end
       end
     end
