b44fb24314186abbcfef5d690850cf9a8216c08a
Danny Whalen <daniel.r.whalen@gmail.com>
2014-05-26 09:36:14 -0700

    Add feature wrapper fine-grained control over hash access

--- app/workers/publisher_update.rb
+++ app/workers/publisher_update.rb
@@ -3,24 +3,51 @@ module Georelevent
     class PublisherUpdate < Struct.new(:features, :publisher)
       def call
         features.lazy.
+          map(&method(:wrap_feature)).
           map(&method(:build_event)).
           select(&method(:save_event?)).
           force
       end
 
+      def wrap_feature(feature)
+        Feature.new(feature)
+      end
+
       def build_event(feature)
         Event.new do |e|
           e.publisher_id = publisher.id
-          e.feature_id   = feature['id']
-          e.title        = feature['properties']['title']
-          e.description  = feature['properties']['description']
-          e.geom         = feature['geometry'].to_json
+          e.feature_id   = feature.id
+          e.title        = feature.title
+          e.description  = feature.description
+          e.geom         = feature.geometry
         end
       end
 
       def save_event?(event)
         event.save
       end
-    end
-  end
-end
+
+      class Feature < Struct.new(:data)
+        def id
+          data['id']
+        end
+
+        def title
+          properties['title']
+        end
+
+        def description
+          properties['description']
+        end
+
+        def geometry
+          data['geometry'].to_json
+        end
+
+        def properties
+          data['properties'] || {}
+        end
+      end # Feature
+    end # PublisherUpdate
+  end # Workers
+end # Georelevent
