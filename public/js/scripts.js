07c1fc1595e519142102633e56d5309c41fbfd07
Andrew Douglass <andrew@wasteddaylight.com>
2014-06-04 14:05:24 -0500

    Scroll wheel disabled on map to conform with http://content.stamen.com/stamens-checklist-for-maps

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,5 @@
 var map = L.map('map').setView([35.2031535, -80.8395259], 9);
+map.scrollWheelZoom.disable();
 
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/ardouglass.h3mingmm/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
