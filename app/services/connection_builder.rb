2d013b88e60385695f4bd2cb1d5e5c194184df45
Danny Whalen <daniel.r.whalen@gmail.com>
2014-06-10 07:34:17 -0700

    Cleaup request logging logic

--- app/services/connection_builder.rb
+++ app/services/connection_builder.rb
@@ -7,7 +7,7 @@ module ConnectionBuilder
       conn.options.timeout = 5
       conn.headers['Content-Type'] = 'application/json'
       conn.response :json
-      conn.use :instrumentation, name: name
+      conn.use :instrumentation, name: name unless Georelevent::App.test?
       conn.adapter Faraday.default_adapter
     end
   end
@@ -20,9 +20,6 @@ module ConnectionBuilder
       logger.info '[%s] %s %s (%.3f s)' % [url.host, http_method, url.request_uri, duration]
     end
 
-    # log requests unless we're in the test environment
-    if ! Georelevent::App.test?
-      ActiveSupport::Notifications.subscribe /^request\.(publisher|subscription)\./, new(Georelevent::App.logger)
-    end
+    ActiveSupport::Notifications.subscribe /^request\.(publisher|subscription)\./, new(Georelevent::App.logger)
   end
 end
