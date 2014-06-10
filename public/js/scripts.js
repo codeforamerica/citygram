ff0a2ab3fe40211a677efeac7ab3fd68d8418e9a
Andrew Douglass <andrew@wasteddaylight.com>
2014-06-10 13:31:41 -0500

    Fixed indentation

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,21 +2,21 @@ var map = L.map('map').setView([35.2031535, -80.8395259], 9);
 map.scrollWheelZoom.disable();
 
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/ardouglass.h3mingmm/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  maxZoom: 18
 }).addTo(map);
 
 var featureGroup = L.featureGroup().addTo(map);
 
 var drawControl = new L.Control.Draw({
-    edit: {
-      featureGroup: featureGroup
-    }
-  }).addTo(map);
+  edit: {
+    featureGroup: featureGroup
+  }
+}).addTo(map);
 
-  map.on('draw:created', function(e) {
-      featureGroup.addLayer(e.layer);
-  });
+map.on('draw:created', function(e) {
+  featureGroup.addLayer(e.layer);
+});
 
 $("#message-method .btn").click(function(e) {
   e.preventDefault();
@@ -25,6 +25,6 @@ $("#message-method .btn").click(function(e) {
   $("#contact-method").val(id);
   $("#contact-info").attr("placeholder", $(this).data("placeholder"));
   $("#contact-info").attr("type", id);
-
+  
   $("#subscription-info").removeClass("hidden");
 });
