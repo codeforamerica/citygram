88ae47fb8603f4c12099487543eb14215813b375
Danny Whalen <daniel.r.whalen@gmail.com>
2014-06-17 15:43:05 -0700

    Use generic env key names

--- config/error_reporting.rb
+++ config/error_reporting.rb
@@ -2,8 +2,8 @@ require 'airbrake'
 require 'sidekiq'
 
 Airbrake.configure do |config|
-  config.api_key = ENV['ERRBIT_KEY']
-  config.host    = ENV['ERRBIT_HOST']
+  config.api_key = ENV['ERROR_LOG_KEY']
+  config.host    = ENV['ERROR_LOG_HOST']
   config.port    = 443
   config.secure  = true
 end
