9a1ea6456f699c3ede6b17798c638ea9ed995f33
Sam Hashemi <sam@samhashemi.com>
2014-07-02 14:54:08 -0700

    Hookup sinatra assetpack and cleanup directory structure.

--- app/routes/pages.rb
+++ app/routes/pages.rb
@@ -15,6 +15,25 @@ module Citygram
         enable :use_code
       end
 
+      register Sinatra::AssetPack
+
+      assets do
+        serve '/js',     from: 'public/js'
+        serve '/css',    from: 'public/css'
+        serve '/img',    from: 'public/img'
+
+        js :app, [
+          '/js/scripts.js',
+        ]
+
+        css :app, [
+          '/css/styles.css',
+        ]
+
+        js_compression :uglify
+        css_compression :simple
+      end
+
       get '/' do
         erb :index
       end
