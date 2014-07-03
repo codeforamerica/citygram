efa184ffd864e77711fb3a9703be8dd2ed1d1b19
Sam Hashemi <sam@samhashemi.com>
2014-07-02 19:06:09 -0700

    Hookup animations, continue styling.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+  var map = app.hookupMap();
+  app.hookupSteps();
+  
+  // Add geometry to map
+  // Set geom in subscription
+  map.on('draw:created', function(e) {
+    var geometry = featureGroup.addLayer(e.layer);
+    s.subscription.geom = JSON.stringify(geometry.toGeoJSON().features[0].geometry);
+  });
+});
+
+var app = app || {};
+
+app.scrollToElement = function(el) {
+  $('html,body').animate({
+    scrollTop: el.offset().top
+  }, 800);
+};
+
+app.hookupMap = function() {
   var options = {
     zoom: 14,
     center: [35.225803, -80.838625],
@@ -6,4 +26,53 @@ $(document).ready(function() {
     scrollWheelZoom: false,
   };
   var map = L.mapbox.map('map', 'ardouglass.h3mingmm', options);
-});
+
+  // Initialise the FeatureGroup to store editable layers
+  var drawnItems = new L.FeatureGroup();
+  map.addLayer(drawnItems);
+
+  // Initialise the draw control and pass it the FeatureGroup of editable layers
+  var drawControl = new L.Control.Draw({
+    edit: { featureGroup: drawnItems }
+  });
+  map.addControl(drawControl);
+
+  return map;
+};
+
+app.hookupSteps = function() {
+  $('.startButton').on('click', function() {
+    app.scrollToElement($('#step1'));
+  });
+
+  $('.somethingButton').on('click', function() {
+    app.scrollToElement($('#step2'));
+  });
+
+  $('.mapButton').on('click', function() {
+    app.scrollToElement($('#step3'));
+  });
+
+  $('.smsButton').on('click', function(event) {
+    $(event.target).addClass('selected');
+    $('.extraInfo').slideDown();
+  });
+
+  $('.subscribeButton').on('click', function(event) {
+    // TODO: animate the done checkmark at the same time
+    $('#confirmation').slideDown();
+    app.scrollToElement($('#confirmation'));
+  });
+
+  $('.resetButton').on('click', function(event) {
+    app.scrollToElement($('#step1'));
+
+    // Hide after we've scrolled up.
+    setTimeout(function() {
+      $('#confirmation').hide();
+    }, 800);
+  });
+};
+
+
+
