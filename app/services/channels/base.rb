23d1f8c71e99761d7a21853a0dc36ee28b8f33ad
Danny Whalen <daniel.r.whalen@gmail.com>
2014-07-10 00:30:17 -0700

    Add configuration option to suppress notifications

--- app/services/channels/base.rb
+++ app/services/channels/base.rb
@@ -3,11 +3,25 @@ module Citygram
     module Channels
       class Base < Struct.new(:subscription, :event)
         def self.call(subscription, event)
-          new(subscription, event).call
+          instance = new(subscription, event)
+
+          if ENV['SUPPRESS_NOTIFICATIONS']
+            instance.suppress
+          else
+            instance.call
+          end
         end
 
         def call
           raise 'abstract - must subclass'
+        end
+
+        def suppress
+          Citygram::App.logger.info 'SUPPRESSED: class=%s subscription_id=%s event_id=%s' % [
+            self.class.name,
+            subscription.id,
+            event.id
+          ]
         end
       end
     end
