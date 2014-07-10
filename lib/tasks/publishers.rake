3837f88b65edcc7fed9556db09b2aa6054c00553
Danny Whalen <daniel.r.whalen@gmail.com>
2014-07-09 23:51:54 -0700

    Only poll active publishers

--- lib/tasks/publishers.rake
+++ lib/tasks/publishers.rake
@@ -1,6 +1,6 @@
 namespace :publishers do
   task update: :app do
-    Publisher.select(:id).paged_each do |publisher|
+    Publisher.where(active: true).select(:id).paged_each do |publisher|
       Citygram::Workers::PublisherPoll.perform_async(publisher.id)
     end
   end
--- spec/tasks/publishers_tasks_spec.rb
+++ spec/tasks/publishers_tasks_spec.rb
@@ -3,7 +3,8 @@ load 'lib/tasks/publishers.rake'
 
 describe Rake::Task['publishers:update'] do
   it 'queues a job for each publisher' do
-    create_list(:publisher, 3)
-    expect{ subject.invoke }.to change{ Citygram::Workers::PublisherPoll.jobs.count }.by(+3)
+    create_list(:publisher, 2, active: true)
+    create(:publisher, active: false)
+    expect{ subject.invoke }.to change{ Citygram::Workers::PublisherPoll.jobs.count }.by(+2)
   end
 end
