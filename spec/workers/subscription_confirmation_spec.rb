10083e2cd14edf10fc00c6eb2060e8d5c549c537
Danny Whalen <daniel.r.whalen@gmail.com>
2014-08-20 18:28:16 -0700

    Fix spec

--- spec/workers/subscription_confirmation_spec.rb
+++ spec/workers/subscription_confirmation_spec.rb
@@ -8,16 +8,18 @@ describe Citygram::Workers::SubscriptionConfirmation do
     let!(:subscription) { create(:subscription, channel: 'sms', phone_number: '212-555-1234') }
 
     before do
+      body = "Welcome! You are now subscribed to #{publisher.title} in #{publisher.city}. Woohoo! If you'd like to give feedback, text back with your email. To unsubscribe from all messages, reply STOP."
+
       stub_request(:post, "https://dev-account-sid:dev-auth-token@api.twilio.com/2010-04-01/Accounts/dev-account-sid/Messages.json").
         with(body: {
-          "Body"=>"Welcome! You are now subscribed to #{publisher.title} in #{publisher.city}. Woohoo! If you'd like to give feedback, text back with your email. To unsubcribe from all messages, reply STOP.",
+          "Body" => body,
           "From"=>"15555555555",
           "To"=>"212-555-1234"}).
         to_return(status: 200, body: {
           'sid' => 'SM10ea1dce707f4bedb44204c9fbc02e39',
           'to' => subscription.phone_number,
           'from' => '15555555555',
-          'body' => "You are now subscribed to #{subscription.publisher.title} in #{subscription.publisher.city}. Woohoo! If you'd like to give feedback, text back with your email. To unsubcribe from all messages, reply STOP.",
+          'body' => body,
           'status' => 'queued'
         }.to_json)
     end
