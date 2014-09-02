d04685196a2fbf21611f2673f657e61d53a0de13
Danny Whalen <daniel.r.whalen@gmail.com>
2014-09-02 13:17:04 -0700

    Add publishers/:publisher_id/events_count
    Returns the count of events for a publisher. Optionally pass
    a `since` date parameter to return the count after the given value.
    Optionally pass a GeoJSON `geometry` parameter to return the count
    of events intersecting the given value.

--- app/routes/publishers.rb
+++ app/routes/publishers.rb
@@ -17,5 +17,35 @@ module Citygram::Routes
     get '/publishers' do
       Publisher.dataset.paginate(params[:page], params[:per]).order(Sequel.desc(:created_at))
     end
+
+    desc <<-DESC
+      Count of events by a given publisher
+    DESC
+
+    params do
+      requires :publisher_id, type: Integer
+      optional :geometry, type: String
+      optional :since, type: DateTime
+    end
+
+    get '/publishers/:publisher_id/events_count' do
+      query = Event.where(publisher_id: params[:publisher_id])
+      
+      if since = params[:since]
+        query = query.where('created_at > ?', since)
+      end
+
+      if geometry = params[:geometry]
+        geom = GeoRuby::GeojsonParser.new.parse(geometry)
+        query = query.where('ST_Intersects(geom, ?::geometry)', geom.as_ewkt)
+      end
+
+      {
+        events_count: query.count,
+        publisher_id: params[:publisher_id],
+        geometry: geometry,
+        since: since,
+      }
+    end
   end
 end
--- spec/routes/publishers_spec.rb
+++ spec/routes/publishers_spec.rb
@@ -17,4 +17,31 @@ describe Citygram::Routes::Publishers do
       expect(last_response.body).to eq [publishers[2], publishers[1]].to_json
     end
   end
+
+  describe 'GET /publishers/:publisher_id/events_count' do
+    let(:params) {{ since: since, geometry: geometry }}
+    let(:geometry) { '{"type":"Polygon","coordinates":[[[-80.93490600585938,35.263561862152095],[-80.69320678710938,35.32745068492882],[-80.60531616210938,35.14124815600257],[-80.83328247070312,35.06597313798418],[-80.93490600585938,35.263561862152095]]]}' }
+    let(:since) { 7.days.ago.iso8601 }
+    let(:publisher) { create(:publisher) }
+
+    it 'counts events by the publisher since a given date, intersecting a given geometry' do
+      create(:event, publisher_id: publisher.id, geom: '{"type":"Point","coordinates":[0.0,0.0]}')
+      create(:event, publisher_id: publisher.id, created_at: 2.weeks.ago, geom: '{"type":"Point","coordinates":[-80.898822,35.22454]}')
+
+      create(:event, publisher_id: publisher.id, geom: '{"type":"Point","coordinates":[-80.898822,35.22454]}')
+      create(:event, publisher_id: publisher.id, created_at: 2.weeks.ago, geom: '{"type":"Point","coordinates":[0.0,0.0]}')
+
+      get "/publishers/#{ publisher.id }/events_count", params
+
+      resp = {
+        events_count: 1,
+        publisher_id: publisher.id,
+        geometry: geometry,
+        since: since,
+      }
+
+      expect(last_response.body).to eq resp.to_json
+      expect(last_response.status).to eq 200
+    end
+  end
 end
