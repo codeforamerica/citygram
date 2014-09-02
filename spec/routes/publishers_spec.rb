613a182d34d7e75c174f3a3d30a23be4a8d5fd12
Danny Whalen <daniel.r.whalen@gmail.com>
2014-09-02 13:32:51 -0700

    Organize spec setup data

--- spec/routes/publishers_spec.rb
+++ spec/routes/publishers_spec.rb
@@ -25,10 +25,10 @@ describe Citygram::Routes::Publishers do
     let(:publisher) { create(:publisher) }
 
     it 'counts events by the publisher since a given date, intersecting a given geometry' do
-      create(:event, publisher_id: publisher.id, geom: '{"type":"Point","coordinates":[0.0,0.0]}')
-      create(:event, publisher_id: publisher.id, created_at: 2.weeks.ago, geom: '{"type":"Point","coordinates":[-80.898822,35.22454]}')
-
       create(:event, publisher_id: publisher.id, geom: '{"type":"Point","coordinates":[-80.898822,35.22454]}')
+      create(:event, publisher_id: publisher.id, geom: '{"type":"Point","coordinates":[0.0,0.0]}')
+
+      create(:event, publisher_id: publisher.id, created_at: 2.weeks.ago, geom: '{"type":"Point","coordinates":[-80.898822,35.22454]}')
       create(:event, publisher_id: publisher.id, created_at: 2.weeks.ago, geom: '{"type":"Point","coordinates":[0.0,0.0]}')
 
       get "/publishers/#{ publisher.id }/events_count", params
