f82f818f75851a98f46fc6773af2b660911fbe8a
Danny Whalen <daniel.r.whalen@gmail.com>
2014-09-14 11:26:50 -0400

    Only queue digest email for active subscriptions

--- spec/tasks/digests_tasks_spec.rb
+++ spec/tasks/digests_tasks_spec.rb
@@ -4,6 +4,7 @@ load 'lib/tasks/digests.rake'
 describe Rake::Task['digests:send'] do
   it 'queues a job for each digest subscription' do
     email_subscription = create(:subscription, channel: 'email', email_address: 'a@b.cc')
+    inactive_email_subscription = create(:subscription, channel: 'email', email_address: 'a@b.cc', unsubscribed_at: DateTime.now)
     sms_subscription = create(:subscription, channel: 'sms', phone_number: '1234567890')
     expect{ subject.invoke }.to change{ Citygram::Workers::Notifier.jobs.count }.by(+1)
   end
