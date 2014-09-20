625ce8da86198d678825a7c0a9a8a83105c4d42d
Danny Whalen <daniel.r.whalen@gmail.com>
2014-09-20 12:57:35 -0700

    Dev view for email - DONT MERGE

--- app/routes/digests.rb
+++ app/routes/digests.rb
@@ -5,5 +5,13 @@ module Citygram::Routes
       @events = Event.from_subscription(@subscription, params)
       erb :digest
     end
+
+    get '/events/email' do
+      content_type 'text/html'
+      @subscription = Subscription.where(channel: 'email').last
+      @events = Event.from_subscription(@subscription, Event.date_defaults)
+      template = ERB.new(File.read(File.join(Citygram::App.root, '/app/views/email.erb')))
+      template.result(binding)
+    end
   end
 end
