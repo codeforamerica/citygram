0c15cfde9439542e2883877cedd8576497dac996
Livien Yin <livienyin@gmail.com>
2014-10-22 13:58:41 -0700

    Add splash page content, assets, and styling

--- app/routes/pages.rb
+++ app/routes/pages.rb
@@ -2,7 +2,7 @@ module Citygram::Routes
   class Pages < Citygram::App
     get '/' do
       @publishers = Publisher.visible
-      erb :index
+      erb :index, layout: :'splash-layout'
     end
   end
 end
