61397c748304047d43d977c85a71fd133667d46c
Dirk Kelly <github@dirkkelly.com>
2015-02-11 19:09:13 -0500

    index cleanup

--- app/routes/pages.rb
+++ app/routes/pages.rb
@@ -1,17 +1,8 @@
 module Citygram::Routes
   class Pages < Citygram::App
     get '/' do
-      @cities = City.all.select do |city|
-        Publisher.active.visible.tagged(city.id).any?
-      end
-
-      erb :index, layout: false
-    end
-
-    get '/:tag' do
-      @city = City.find(params[:tag])
-      @publishers = Publisher.active.visible.tagged(@city.id)
-      erb :show
+      @publishers = Publisher.active
+      erb :index
     end
   end
 end
