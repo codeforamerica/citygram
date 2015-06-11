b182e0ff8b8e52ea642e7ad93d5d905745c048a7
Michael duPont <michael@mdupont.com>
2015-06-11 01:25:35 -0400

    Added landing page to index.php

--- orlando-dev/intermediary/index.php
+++ orlando-dev/intermediary/index.php
@@ -4,8 +4,19 @@
 	
 	if (isset($_GET['service'])) {
 		$execString .= " {$_GET['service']}";
+		$output = exec($execString);
+	} else {
+		$formatType = 'text/html';
+		$output = '
+		<h1>Orlando Citygram API</h1>
+		<p>Hi. My job is to take data streams from the Orlando public data portal and format them to be Citygram compliant. Here is a list of service urls that are publically supported:</p>
+		<p>
+			<a href="http://orlando-citygram-api.azurewebsites.net/?service=landmarks">http://orlando-citygram-api.azurewebsites.net/?service=landmarks</a><br>
+			<a href="http://orlando-citygram-api.azurewebsites.net/?service=police">http://orlando-citygram-api.azurewebsites.net/?service=police</a>
+		</p>
+		<p>This is a project by <a href="http://codefororlando.com">Code For Orlando</a>. For any issues, contact Michael duPont at <a href="mailto:michael@mdupont.com">michael@mdupont.com</a></p>
+		';
 	}
-	$output = exec($execString);
 	
 	header("Access-Control-Allow-Origin: *");
 	header("Content-Type: {$formatType}");
--- orlando-dev/readme.md
+++ orlando-dev/readme.md
@@ -14,3 +14,5 @@ This section is for Code For Orlando development and testing.
 * <b>Icons</b> - icons to be displayed with service/topic options
 * <b>Intermediary</b> - API service that pulls data from Orlando data portal and formats to be city gram compliant
 * <b>orl_endpoint_creation.rb</b> - Ruby script to create Publisher entries in the primary db
+
+The intermediary API is currently hosted at http://orlando-citygram-api.azurewebsites.net
