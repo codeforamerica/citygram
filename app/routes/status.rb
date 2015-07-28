1d8ad182b404e9118c599c9d9cf95f486e3cef85
Mason Packard <mason.packard@yahoo.com>
2015-07-28 10:44:55 -0400

    Changing over to JSON response and removing human view.

--- app/routes/status.rb
+++ app/routes/status.rb
@@ -1,37 +1,41 @@
 module Citygram::Routes
-  class Status < Citygram::App
-    # def check
-    #   response_hash = Hash.new
-    #   #{ :status => "ok", :updated => "", :dependencies => "", :resources => "" }
-    #   response_hash[:dependencies] = [ "postgres", "s3" ]
-    #   response_hash[:status] = everything_ok? ? "ok" : "NOT OK"
-    #   response_hash[:updated] = Time.now.to_i
-    #   response_hash[:resources] = {}
-    #   render :inline => response_hash.to_json
-    # end
+  class Status < Grape::API
+    version 'v1', using: :header, vendor: 'citygram'
+    format :json
 
-    def everything_ok?
-      # Check that we have some database presence and core data is available
-      database_okay? && twilio_response_okay?
-    end
+    desc 'Check Status of Application'
 
-    def database_okay?
-      Publisher.first.present?
-    end
+    helpers do
+      def check
+        everything_ok? ? "ok" : "NOT OK"
+      end
 
-    def twilio_response_okay?
-      uri = URI('https://api.twilio.com/2010-04-01')
-      res = Net::HTTP.get_response(uri)
-      if res.code == "200"
-        true
-      else
-        false
+      def everything_ok?
+        # Check that we have some database presence and core data is available
+        database_okay? && twilio_response_okay?
+      end
+
+      def database_okay?
+        Publisher.first.present?
+      end
+
+      def twilio_response_okay?
+        uri = URI('https://api.twilio.com/2010-04-01')
+        res = Net::HTTP.get_response(uri)
+        if res.code == "200"
+          true
+        else
+          false
+        end
       end
     end
 
     get '/status' do
-      # status_check = "yes"
-      erb :status, :locals => {:status_check => everything_ok?}
+      {
+        status: check,
+        dependencies: [ "postgres", "twilio" ],
+        updated: Time.now.to_i
+      }
     end
   end
 end
