0bccb35c004c77afdd7505ab599b4755988e8065
Mason Packard <mason.packard@yahoo.com>
2015-07-28 19:02:36 -0400

    Changing the application status to be outside of the api and simply respond with json.

--- app/routes/status.rb
+++ app/routes/status.rb
@@ -1,32 +1,25 @@
 module Citygram::Routes
-  class Status < Grape::API
-    version 'v1', using: :header, vendor: 'citygram'
-    format :json
+  class Status < Citygram::App
+    def check
+      everything_ok? ? "ok" : "NOT OK"
+    end
 
-    desc 'Check Status of Application'
+    def everything_ok?
+      # Check that we have some database presence and core data is available
+      database_okay? && twilio_response_okay?
+    end
 
-    helpers do
-      def check
-        everything_ok? ? "ok" : "NOT OK"
-      end
+    def database_okay?
+      Publisher.first.present?
+    end
 
-      def everything_ok?
-        # Check that we have some database presence and core data is available
-        database_okay? && twilio_response_okay?
-      end
-
-      def database_okay?
-        Publisher.first.present?
-      end
-
-      def twilio_response_okay?
-        uri = URI('https://api.twilio.com/2010-04-01')
-        res = Net::HTTP.get_response(uri)
-        if res.code == "200"
-          true
-        else
-          false
-        end
+    def twilio_response_okay?
+      uri = URI('https://api.twilio.com/2010-04-01')
+      res = Net::HTTP.get_response(uri)
+      if res.code == "200"
+        true
+      else
+        false
       end
     end
 
@@ -35,7 +28,7 @@ module Citygram::Routes
         status: check,
         dependencies: [ "postgres", "twilio" ],
         updated: Time.now.to_i
-      }
+      }.to_json
     end
   end
 end
