a283f479f5426a79b0f1b470789c451f70c71fb4
Jesse Szwedko <jesse.szwedko@gmail.com>
2018-07-16 15:31:50 -0700

    Rely on application to run database migrations
    Rather than building separate image.

--- docker/data/Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM mdillon/postgis
-
-COPY db/schema.sql /tmp/schema.sql
-COPY docker/data/schema.sh /docker-entrypoint-initdb.d/schema.sh
--- docker/data/schema.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-createdb -U postgres citygram_test
-createdb -U postgres citygram_development
-
-psql -U postgres citygram_test < /tmp/schema.sql
-psql -U postgres citygram_development < /tmp/schema.sql
