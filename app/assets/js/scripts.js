efe0ea846e10c8fd07acce8f087361f41455a494
Jim Van Fleet <jim@jimvanfleet.com>
2019-04-09 19:04:28 -0400

    Adding Google Maps key

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -350,8 +350,10 @@ app.scrollToElement = function(el) {
 
 app.geocode = function(address, city, state, callback, context) {
    var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodeURIComponent(address);
+      url += '&key=AIzaSyCeF_VBd1M0V4ErSBbeerMgSyhalX9PIeM';
       url += '&components=locality:' + encodeURIComponent(city);
       url += '|administrative_area:' + encodeURIComponent(state);
+      url += '|administrative_area:' + encodeURIComponent(state);      
 
   $.getJSON(url, function(response) {
     if (response.error || response.results.length === 0) {
